3c99402c0b409786ddf5023b6d31b0d2652b84b7
asljivo1 <asljivo1@etf.unsa.ba>
2018-07-04 14:48:31 +0200

    cycle80_bi25600_5slots_csb1-0_1000s_seed1

--- ahsim.sh
+++ ahsim.sh
@@ -1,4 +1,4 @@
 clear 
-./waf --run "test --BeaconInterval=25600 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=500 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-loops.txt\"  --TrafficInterval=1000 --CycleTime=80 --nControlLoops=1 --DataMode=\"MCS2_0\" --seed=5" -j4
+./waf --run "test --BeaconInterval=25600 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=500 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-loops.txt\"  --TrafficInterval=1000 --CycleTime=80 --nControlLoops=1 --DataMode=\"MCS2_0\" --seed=1" -j4
 
 #./waf --run test --command-template="gdb --args %s <args> --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=1 --payloadSize=300 --rho=400 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-loops.txt\"  --TrafficInterval=500 --CycleTime=500 --nControlLoops=2 " -j4
