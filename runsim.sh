a680e4717d720d95a42b7e550dbe546e667471bc
asljivo1 <asljivo1@etf.unsa.ba>
2018-09-05 05:02:54 +0200

    2 loops, 1 simulation for 64,160,512 132,162,182 stations

--- runsim.sh
+++ runsim.sh
@@ -1,16 +1,16 @@
 
-./waf --run "test --BeaconInterval=70656 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-172-3-1-70656-0-31.txt\"  --TrafficInterval=1000 --CycleTime=160 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
+#./waf --run "test --BeaconInterval=70656 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-172-3-1-70656-0-31.txt\"  --TrafficInterval=1000 --CycleTime=160 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
 
-#./waf --run "test --BeaconInterval=70656 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-162-3-1-70656-0-31.txt\"  --TrafficInterval=1000 --CycleTime=160 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
-
-
-
-#./waf --run "test --BeaconInterval=29696 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-132-3-1-29696-0-31.txt\"  --TrafficInterval=1000 --CycleTime=64 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
-
-./waf --run "test --BeaconInterval=29696 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-142-3-1-29696-0-31.txt\"  --TrafficInterval=1000 --CycleTime=64 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
+./waf --run "test --BeaconInterval=70656 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-162-3-1-70656-0-31.txt\"  --TrafficInterval=1000 --CycleTime=160 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
 
 
 
-#./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-182-3-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=512 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
+./waf --run "test --BeaconInterval=29696 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-132-3-1-29696-0-31.txt\"  --TrafficInterval=1000 --CycleTime=64 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
 
-./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-202-3-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=512 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
+#./waf --run "test --BeaconInterval=29696 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-142-3-1-29696-0-31.txt\"  --TrafficInterval=1000 --CycleTime=64 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
+
+
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-182-3-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=512 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
+
+#./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=300 --payloadSize=64 --rho=300 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-202-3-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=512 --nControlLoops=2 --DataMode=\"MCS2_0\" --seed=1" -j4
